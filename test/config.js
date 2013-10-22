4de874dca0d035516f042e34b33184dc1372e9f4
Christoph Dorn <christoph@christophdorn.com>
2013-10-21 22:44:48 -0700

    initial parsing

--- test/config.js
+++ test/config.js
@@ -1,5 +1,6 @@
 
 const PATH = require("path");
+const FS = require("fs-extra");
 const ASSERT = require("assert");
 const CONFIG = require("../lib/config");
 
@@ -10,6 +11,38 @@ const DEBUG = false;
 
 describe('config', function() {
 
+	describe('can load', function() {
 
+		it('compact json-ld', function(done) {
+
+			return CONFIG.fromFile(PATH.join(__dirname, "assets/various/fred.ld.json"), function(err, config) {
+				if (err) return done(err);
+				ASSERT.equal(typeof config, "object");
+				ASSERT.equal(config.warnings.length, 0);
+				ASSERT.deepEqual(config.activity, [
+					["init", FS.readJsonSync(PATH.join(__dirname, "assets/various/fred.ld.json"))]
+				]);
+				return done();
+			});
+
+		});
+
+		it('non json-ld', function(done) {
+
+			return CONFIG.fromFile(PATH.join(__dirname, "assets/various/fred.json"), function(err, config) {
+				if (err) return done(err);
+				ASSERT.equal(typeof config, "object");
+				ASSERT.deepEqual(config.warnings, [
+					['JSONLD.expand()', 'NO_DATA']
+				]);
+				ASSERT.deepEqual(config.activity, [
+					["init", FS.readJsonSync(PATH.join(__dirname, "assets/various/fred.json"))]
+				]);
+				return done();
+			});
+
+		});
+
+	});
 
 });
