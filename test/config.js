b0a5acab595f236721d9126f763dfd2caaccc2d2
Christoph Dorn <christoph@christophdorn.com>
2013-10-21 19:48:05 -0700

    initial commit

---
+++ test/config.js
@@ -0,0 +1,15 @@
+
+const PATH = require("path");
+const ASSERT = require("assert");
+const CONFIG = require("../lib/config");
+
+//const MODE = "test";
+const MODE = "write";
+
+const DEBUG = false;
+
+describe('config', function() {
+
+
+
+});
